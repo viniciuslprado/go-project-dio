9e0b08eea1f47bcfc0eca1452e51f42a272926d2
Vinicius <163932214+viniciuslprado@users.noreply.github.com>
2024-11-04 15:15:49 -0300

    Add files via upload
    "Programa para converter o valor do ponto de ebulição da água de Kelvin para graus Celsius."

---
+++ Desafio1.go
@@ -0,0 +1,13 @@
+/*Um professor de Ensino Médio solicitou aos seus alunos que desenvolvessem um programa 
+para converter o valor do ponto de ebulição da água de Kelvin para graus Celsius*/
+package main
+
+import "fmt"
+
+const ebulicaoK = 373.2 
+
+func main() {
+	tempk := ebulicaoK
+	tempc := tempk - 273.2
+	fmt.Printf("A temperatura da ebulição da agua em Kelvin é %g, e em Celsius é %g .", tempk, tempc)
+}
